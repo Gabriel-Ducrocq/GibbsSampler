f133e9bec8a02b022e647e78cd61bbf56da58354
Gabriel Ducrocq <gabrielducrocq.maths@gmail.com>
2020-09-12 15:01:17 +0200

    fixing bug

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     data_path = config.scratch_path + "/data/non_isotropic_runs/skymap/skymap.npy"
     d = np.load(data_path, allow_pickle=True)
     d = d.item()
-    pix_map = d["d_"]
+    pix_map = d["pix_map"]
 
     #range_l = np.array([l*(l+1)/(2*np.pi) for l in range(config.L_MAX_SCALARS+1)])
     #plt.plot(cls_[0]*range_l)
