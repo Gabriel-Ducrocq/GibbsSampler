4b023dd66b52cc8aa91307449067273361928bbf
Gabriel Ducrocq <gabrielducrocq.maths@gmail.com>
2020-11-22 22:14:33 +0100

    fixed bug, checking tuning now

--- config.py
+++ config.py
@@ -314,33 +314,15 @@ else:
 
     bl = blocks["BB"][:-1]
     proposal_variances_nc_polarized["BB"][bl[-2]:] *= 3.5
-    #proposal_variances_nc_polarized["BB"][bl[-4]:bl[-2]] *= 1.7
-    proposal_variances_nc_polarized["BB"][bl[-4]:bl[-2]] *= 100000
+    proposal_variances_nc_polarized["BB"][bl[-4]:bl[-2]] *= 1.7
     proposal_variances_nc_polarized["BB"][bl[-11]:bl[-4]] *= 2.5
     proposal_variances_nc_polarized["BB"][bl[-14]:bl[-11]] *= 0.5
-    #proposal_variances_nc_polarized["BB"][bl[-17]:bl[-16]] *= 0.7
-    proposal_variances_nc_polarized["BB"][bl[-17]:bl[-16]] *= 100000
-    proposal_variances_nc_polarized["BB"][bl[-22]:bl[-21]] *= 1
-    #proposal_variances_nc_polarized["BB"][bl[-25]:bl[-22]] *= 0.7
-    proposal_variances_nc_polarized["BB"][bl[-25]:bl[-22]] *= 1000000
+    proposal_variances_nc_polarized["BB"][bl[-17]:bl[-16]] *= 0.7
+    proposal_variances_nc_polarized["BB"][bl[-22]:bl[-21]] *= 3
+    proposal_variances_nc_polarized["BB"][bl[-25]:bl[-22]] *= 0.7
     proposal_variances_nc_polarized["BB"][bl[-26]:bl[-25]] *= 0.9
     proposal_variances_nc_polarized["BB"][bl[-31]:bl[-30]] *= 5
 
     proposal_variances_nc_polarized["EE"] = binned_variances_pol["EE"][2:]
     proposal_variances_nc_polarized["BB"] = binned_variances_pol["BB"][2:]
 
-    """
-    print(proposal_variances_nc_polarized["BB"][bl[-2]:])
-    print(len(proposal_variances_nc_polarized["BB"]))
-    print(len(bins["BB"]))
-    print(bl[-2:])
-    print(len(bl))
-    print(proposal_variances_nc_polarized["BB"][bl[-2]:])
-    print(proposal_variances_nc_polarized["BB"][-2:])
-
-
-    print("\n\n\n")
-    print(bl[-31])
-    print(proposal_variances_nc_polarized["BB"][bl[-31]:bl[-30]])
-    print(proposal_variances_nc_polarized["BB"][2:280])
-    """
