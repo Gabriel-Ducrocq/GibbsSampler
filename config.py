b08098952381650f22eea65e186341e61005a108
Gabriel Ducrocq <gabrielducrocq.maths@gmail.com>
2020-11-30 21:54:23 +0100

    again

--- config.py
+++ config.py
@@ -326,7 +326,7 @@ else:
     proposal_variances_nc_polarized["BB"][bl[-22]:bl[-21]] *= 12
     proposal_variances_nc_polarized["BB"][bl[-25]:bl[-22]] *= 0.7
     proposal_variances_nc_polarized["BB"][bl[-26]:bl[-25]] *= 0.9
-    proposal_variances_nc_polarized["BB"][bl[-30]:bl[-29]] *= 20
+    proposal_variances_nc_polarized["BB"][bl[-30]:bl[-29]] *= 30
     proposal_variances_nc_polarized["BB"][bl[-31]:bl[-30]] *= 0.07
 
     proposal_variances_nc_polarized["EE"] = binned_variances_pol["EE"][2:]
