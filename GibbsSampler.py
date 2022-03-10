9287daed1085cc369479c3c9751a480d978c3fb4
Gabriel Ducrocq <gabrielducrocq.maths@gmail.com>
2022-03-10 14:48:42 +0100

    GibbsSampler.py is commented

--- GibbsSampler.py
+++ GibbsSampler.py
@@ -8,8 +8,24 @@ from default_gibbs import sample_cls
 
 
 class GibbsSampler():
-    def __init__(self, pix_map, noise, beam_fwhm_deg, nside, lmax, Npix, polarization = False, bins=None, n_iter = 10000,
+    def __init__(self, pix_map, noise, beam_fwhm_deg, nside, lmax, polarization = False, bins=None, n_iter = 10000,
                  gibbs_cr = False, rj_step = False):
+        """
+        This is the basic Gibbs sampler class, from which any variant will inherit.
+        The set of alm coefficients is in m major, as expected by the healpy library.
+
+        :param pix_map: array of size Npix, observed pixel map, called d in the equations
+        :param noise: float, noise level. This assumes a noise covariance matrix prop to identity.
+        :param beam_fwhm_deg: float, definition of the beam in degree.
+        :param nside: int, NSIDE used to define the grid over the sphere.
+        :param lmax: int, maximum \ell on which we perform inference
+        :param polarization: boolean, whether we are making inference on EE and BB or just TT.
+        :param bins: array of integers, each element is the starting index and ending index of a power spectrum bin.
+        :param n_iter: integer, number of iterations of Gibbs sampler to perform.
+        :param gibbs_cr: whether we use the classical PCG resolution for the constrained realization step, or the augmented
+                variable scheme described in the paper.
+        :param rj_step: boolean,  whether we use the PCG resolution for the CR step or the RJPO algorithm.
+        """
         self.noise = noise
         self.beam = beam_fwhm_deg
         self.nside = nside
@@ -17,7 +33,7 @@ class GibbsSampler():
         self.polarization = polarization
         self.bins = bins
         self.pix_map = pix_map
-        self.Npix = Npix
+        self.Npix = 12*nside**2
         self.bl_map = self.compute_bl_map(beam_fwhm_deg)
         self.constrained_sampler = None
         self.cls_sampler = None
@@ -25,6 +41,8 @@ class GibbsSampler():
         self.gibbs_cr = gibbs_cr
         self.rj_step = rj_step
         if bins is None:
+            ## If the user provides no binning scheme, then each bin is made of only one \ell. This equivalent to
+            ## no binning.
             if not polarization:
                 self.bins = np.array([l for l in range(lmax+2)])
             else:
@@ -33,26 +51,48 @@ class GibbsSampler():
         else:
             self.bins = bins
 
+        # This is the array we use to convert D_\ell to C_\ell
         self.dls_to_cls_array = np.array([2*np.pi/(l*(l+1)) if l !=0 else 0 for l in range(lmax+1)])
 
     def dls_to_cls(self, dls_):
+        """
+
+        :param dls_: array of floats, representing the D_\ells
+        :return: array of floats, representing the C_\ells
+        """
         return dls_[:]*self.dls_to_cls_array
 
-    def compute_bl_map(self, beam_fwhm):
-        fwhm_radians = (np.pi / 180) * beam_fwhm
+    def compute_bl_map(self, beam_fwhm_deg):
+        """
+
+        :param beam_fwhm_deg: float, definition of the beam in degree
+        :return: array of floats, the b_\ells expension over all the spherical harmonics coefficients. This is the diagonal
+                of the B matrix in the paper.
+        """
+        fwhm_radians = (np.pi / 180) * beam_fwhm_deg
         bl_gauss = hp.gauss_beam(fwhm=fwhm_radians, lmax=self.lmax)
         bl_map = np.concatenate([bl_gauss,np.array([cl for m in range(1, self.lmax + 1) for cl in bl_gauss[m:] for _ in range(2)])])
         return bl_map
 
     def run_temperature(self, dls_init):
+        """
+        This function runs the Gibbs sampler on temperature only.
+
+        :param dls_init: array of floats, initial binned D_\ell of the Gibbs sampler.
+        :return: array of float, size (n_iter, lmax), being the trajectory of the Gibbs sampler.
+                array of float, size n_iter, being the history of acceptances of the contrained realization algorithm being used.
+                array of float, size n_iter, being the the history of cpu execution time in second for each iteration.
+        """
         h_accept_cr = []
         h_dls = []
         h_time_seconds = []
         binned_dls = dls_init
-        dls = utils.unfold_bins(binned_dls, config.bins)
-        cls = self.dls_to_cls(dls)
-        var_cls_full = utils.generate_var_cl(dls)
-        skymap, accept = self.constrained_sampler.sample(cls[:], var_cls_full.copy(), None, metropolis_step=False)
+        dls = utils.unfold_bins(binned_dls, config.bins) #Expending the binned D_\ell to the unbinned size vector of D_\ell
+        cls = self.dls_to_cls(dls) # turn D_\ell to C_\ell
+        var_cls_full = utils.generate_var_cl(dls) # Expand the array of C_\ell into an array of the size of alm array, m major.
+        skymap, accept = self.constrained_sampler.sample(cls[:], var_cls_full.copy(), None, metropolis_step=False) #make a first CR step.
+        ## Note that this first CR step is useless if we use a regular PCG in the CR step in the loop. Otherwise, we actually need an
+        ## intialization of the map before the first iteration, and it is natural to make a first CR step to get it.
         h_dls.append(binned_dls)
         for i in range(self.n_iter):
             if i % 1 == 0:
@@ -61,12 +101,13 @@ class GibbsSampler():
 
             start_time = time.process_time()
             skymap, accept = self.constrained_sampler.sample(cls[:], var_cls_full.copy(), skymap, metropolis_step=False,
-                                                             use_gibbs=False)
-            binned_dls = self.cls_sampler.sample(skymap[:])
-            dls = utils.unfold_bins(binned_dls, self.bins)
-            cls = self.dls_to_cls(dls)
-            var_cls_full = utils.generate_var_cl(dls)
+                                                             use_gibbs=False) #Make a CR step. first step of Gibbs sampler.
+            binned_dls = self.cls_sampler.sample(skymap[:]) #Sample the binned D_\ells, second step of Gibbs sampler
+            dls = utils.unfold_bins(binned_dls, self.bins) #Unfold the binned D_\ell
+            cls = self.dls_to_cls(dls) #Turn it into C_\ell
+            var_cls_full = utils.generate_var_cl(dls) # Expand the array of C_\ell
 
+            ##Then we keep all the information in memory for output:
             h_accept_cr.append(accept)
             end_time = time.process_time()
             h_dls.append(binned_dls)
@@ -76,14 +117,25 @@ class GibbsSampler():
         return np.array(h_dls), np.array(h_accept_cr), h_time_seconds
 
     def run_polarization(self, dls_init):
+        """
+        This one runs the Gibbs sampler on EE and BB only.
+
+        :param dls_init: dictionnary of arrays of float, {"EE":array1, "BB":array2}, arrays being the binned D_\ell of polarization.
+        :return: dict of array of float, size (n_iter, lmax), being the trajectory of the Gibbs sampler.
+                array of float, size n_iter, being the history of acceptances of the contrained realization algorithm being used.
+                array of float, size n_iter, being the the history of cpu execution time in second for the CR step.
+                array of float, size n_iter, being the the history of cpu execution time in second for the Pow Spec sampling step.
+        """
+
+        #Intialize everything
         h_accept_cr = []
         h_duration_cr = []
         h_duration_cls_sampling = []
         h_dls = {"EE":[], "BB":[]}
         binned_dls = dls_init
         dls_unbinned = {"EE":utils.unfold_bins(binned_dls["EE"].copy(), self.bins["EE"]), "BB":utils.unfold_bins(binned_dls["BB"].copy(), self.bins["BB"])}
-        print("RJ_STEP ?:", self.rj_step)
         if self.rj_step == True or self.gibbs_cr == True:
+            ## If we use a RJPO algo or auxiliary variable scheme instead of regular PCG, we need this initialization of the skymap.
             skymap, accept = self.constrained_sampler.sample(dls_unbinned)
 
         h_dls["EE"].append(binned_dls["EE"])
@@ -94,15 +146,11 @@ class GibbsSampler():
                 print(i)
 
             start_time = time.clock()
-            #if self.rj_step == False:
-            #    skymap, accept = self.constrained_sampler.sample(dls_unbinned.copy())
-            #else:
-            #    skymap, accept = self.constrained_sampler.sample(dls_unbinned.copy(), skymap)
-            #    h_accept_cr.append(accept)
-
             if self.rj_step is False and self.gibbs_cr is False:
+                ## If we the PCG, CR step
                 skymap, _ = self.constrained_sampler.sample(dls_unbinned.copy())
             else:
+                ## If we use the PCG or the auxiliary variable scheme, different CR step
                 skymap, accept = self.constrained_sampler.sample(dls_unbinned.copy(), skymap)
                 h_accept_cr.append(accept)
 
@@ -111,10 +159,11 @@ class GibbsSampler():
             h_duration_cr.append(duration)
 
             start_time = time.clock()
-            binned_dls = self.cls_sampler.sample(skymap.copy())
+            binned_dls = self.cls_sampler.sample(skymap.copy()) # Sample the binned D_\ell
             end_time = time.clock()
             duration =end_time - start_time
             h_duration_cls_sampling.append(duration)
+            ## Unbin the power spectrum for the next iteration:
             dls_unbinned = {"EE":utils.unfold_bins(binned_dls["EE"].copy(), self.bins["EE"]), "BB":utils.unfold_bins(binned_dls["BB"].copy(), self.bins["BB"])}
             
             h_dls["EE"].append(binned_dls["EE"])
@@ -129,6 +178,11 @@ class GibbsSampler():
 
 
     def run(self, dls_init):
+        """
+        This function runs either the temperature or polarization only Gibbs sampler.
+        :param dls_init: initial binned D_\ell
+        :return: return whatever the run_temperature/polarization returns.
+        """
         if not self.polarization:
             return self.run_temperature(dls_init)
         else:
